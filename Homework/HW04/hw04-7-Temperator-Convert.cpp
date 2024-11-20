f28b4baf84251ad438e677065f92c6937ea61ea4
Zag Chakrit <85066044+Zag5A6167@users.noreply.github.com>
2024-11-20 21:20:36 +0700

    Add files via upload

---
+++ Homework/HW04/hw04-7-Temperator-Convert.cpp
@@ -0,0 +1,36 @@
+/*
+    จงเขียนโปรแกรมเพื่อแปลงอุณหภูมิ โดยรับมาจากผู้ใช้เป็นองศาเซลเซียส และแปลงเป็นฟาเรนไฮต์
+    
+    Test case:
+        User input :
+            22
+    Output:
+        22 degree Celsius = 71.6 degree Fahrenheit
+
+    Test case:
+        User input :
+            36
+    Output:
+        36 degree Celsius = 96.8 degree Fahrenheit
+*/
+
+
+#include <stdio.h>
+
+int celsius ;
+float fahrenheit;
+
+
+int main(){
+
+    printf("User input : ");
+    scanf("%d",&celsius);
+
+    fahrenheit = ((float)celsius * 9/5) + 32;
+
+    printf("%d degree Celsius = %.1f degree Fahrenheit",celsius,fahrenheit);
+
+    return 0;
+}
+
+// Test case ok
