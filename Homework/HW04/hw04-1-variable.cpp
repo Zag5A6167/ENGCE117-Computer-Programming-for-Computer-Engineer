6d9b2b00f2507fa006a36b6665ea43c12f24d4ce
Zag <za12ew44zz_999@hotmail.com>
2024-11-24 11:45:31 +0700

    add hw04-1 flowchart result codex

--- Homework/HW04/hw04-1-variable.cpp
+++ Homework/HW04/hw04-1-variable.cpp
@@ -5,20 +5,18 @@
         10 20.0 Haha a
 */
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main() {
 
     int a = 10 ;
     float b = 20 ;
     char c[] = "Haha" ;
     char d = 'a' ;
-    printf( "%d %.1f %s %c", a, b, c, d) ; //TODO: #10 Display variable a, b, c and d.
+    printf( "%d %.1f %s %c", a, b, c, d ) ; //TODO: #10 Display variable a, b, c and d.
 
 
     return 0;
 
 }
 
-
-// Test Case Ok
