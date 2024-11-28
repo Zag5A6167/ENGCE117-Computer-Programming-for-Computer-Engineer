e03da4d7fa15026d89521c71579837307e1faf29
Zag <za12ew44zz_999@hotmail.com>
2024-11-29 00:52:59 +0700

    add lab1 1-1 result codex

---
+++ lab/lab1/1-1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h> // fix code
+
+int main() {
+
+    int n;
+    scanf( "%d", &n );
+    for( int i = 1; i <= n; i++ ) {
+        printf( "[%d] Hello world\n", i );
+        
+    }//end for
+    
+}//end function
