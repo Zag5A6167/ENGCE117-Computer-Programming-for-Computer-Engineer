49b6a126c456e2837ba6bd105b4f65b5736156fe
Zag <za12ew44zz_999@hotmail.com>
2024-11-29 23:39:01 +0700

    add lab1.3 result and description

---
+++ lab/lab1/1-3.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+int *GetSet( int * ) ;
+
+int main() {
+    int *data, num ;
+    data = GetSet( &num ) ;
+
+    printf("\n" );
+    printf( "Number of elements: %d\n", num ) ;
+
+    for( int i = 0; i < num; i++ ) {
+        printf( "%d ",data[ i ] ) ;
+
+    }//endfor
+
+    
+
+
+    free( data ) ;
+    return 0 ;
+}//end function
+
+
+
+/** 
+  Function Name : GetSet pointer int
+  @Param Input *n   int  data
+ 
+**/
+
+int *GetSet( int *n ) {
+    printf( "Enter the number of elements: " ) ;
+    scanf( "%d", n ) ;
+    int *arr = ( int* )malloc( *n * sizeof( int ) ) ;
+    
+    printf( "Enter the elements: ") ;
+    for(int i = 0 ; i < *n;i++ ) {
+        scanf( "%d",&arr[ i ] ) ;
+    }//end for
+
+
+    return arr ;
+}//end function
