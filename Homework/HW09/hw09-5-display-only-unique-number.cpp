f034b7945a740132a459b79e390f8f2f41ef002a
Zag <za12ew44zz_999@hotmail.com>
2024-12-08 16:57:53 +0700

    add hw09-5

---
+++ Homework/HW09/hw09-5-display-only-unique-number.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n ;
+
+    printf( "Input N: " ) ;
+    scanf( "%d", &n ) ;
+
+    int *arr = ( int* )malloc(n * sizeof( int ) ) ;
+    
+    int cnt[ 100 ] = { 0 } ;
+
+    for (int i = 0; i < n; i++ ) {
+        printf( "Input : " );
+        scanf( "%d", &arr[ i ] ) ;
+       
+    }//end for
+   
+
+
+    for(int i = 0; i < n - 1; i++ ) {
+        int min = i;
+        int temp ;
+        for( int j = i + 1; j < n; j++ ) {
+           if ( arr[ j ] < arr[ min ] ) {
+                min = j;
+            }//end if
+            
+ 
+        }//end for
+
+        temp = arr[ min ] ;
+        arr[ min ] = arr[ i ] ;
+        arr[ i ] = temp ; 
+    }//end for
+
+    
+    for ( int i = 0; i < n; i++ ) {
+        cnt[ arr[ i ] ]++ ;
+
+    }//end for
+
+
+    printf( "Unique value : " );
+    for (int i = 0; i < 100; i++ ) {
+        if ( cnt[ i ] == 1 ) {
+            printf( "%d ", i ) ;
+
+        }//end if
+    }//end for
+
+
+
+   
+
+
+    
+    
+
+    free( arr ) ;
+    return 0 ;
+}//end function
