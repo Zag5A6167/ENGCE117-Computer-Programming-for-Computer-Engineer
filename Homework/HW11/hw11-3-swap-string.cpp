78848907ac0a4e15885a2ede97f0035f615c8611
za12ew44zz <za12ew44zz_999@hotmail.com>
2024-12-09 23:25:09 +0700

    add hw11-3

---
+++ Homework/HW11/hw11-3-swap-string.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char name[ 200 ] ;
+    
+    fgets( name, sizeof( name ), stdin ) ;
+    name[ strcspn( name, "\n" ) ] = '\0' ;
+    
+    int len = strlen(name) ;
+    for ( int i = len - 1; i >= 0; i-- ) {
+        printf( "%c", name[ i ] ) ;
+        
+    }//end function
+    
+    return 0 ;
+}//end function 
