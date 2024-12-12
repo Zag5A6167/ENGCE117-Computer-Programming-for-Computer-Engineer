4c9a3df6994b174dc74a55c0e88432aebaa56eb0
Zag <za12ew44zz_999@hotmail.com>
2024-12-12 07:06:55 +0700

    add labweek3-3

---
+++ lab/lab-week3/lab3-3.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+struct student {
+    char name[ 20 ] ;
+    int age ;
+    char sex ;
+    float gpa ;
+} ;
+
+struct student upgrade( struct student child ) ;
+
+int main() {
+    struct student aboy ;
+    aboy.sex = 'F' ;
+    aboy.gpa = 2.58 ;
+    aboy = upgrade( aboy ) ;
+    printf( "%.2f", aboy.gpa ) ;
+    return 0 ;
+}//end function
+
+
+struct student upgrade( struct student child ) {
+
+    if( child.gpa == 4.00 ) {              // =  4.00 dont cal
+        return child ;
+
+    }//end if
+    else if( child.gpa == 0.00 ) {        // =  0.00 dont cal   
+        return child;    
+
+    }//end else if
+
+    if ( child.sex == 'M' ) {
+         child.gpa += child.gpa * 0.1 ;   // for male 10%
+        
+    }//end if
+    else if( child.sex == 'F' ) {
+        child.gpa += child.gpa * 0.2 ;  // for female 20%
+    }//end if 
+    
+    
+    //printf("in upgrade func gpa is = %.2f\n", ( *child ).gpa ) ;
+    
+    return child ;
+   
+}//end function upgrade
