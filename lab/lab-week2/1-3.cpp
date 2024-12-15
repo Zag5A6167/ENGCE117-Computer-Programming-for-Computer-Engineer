963fde8d771e7750d5c63cb374ab41c05dd8a607
Zag <za12ew44zz_999@hotmail.com>
2024-12-15 13:24:23 +0700

    change

--- lab/lab-week2/1-3.cpp
+++ lab/lab-week2/1-3.cpp
@@ -1,44 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 int *GetSet( int * ) ;
 
+
+
 int main() {
-    int *data, num ;
+
+    int *data, num ;    
     data = GetSet( &num ) ;
 
-    printf("\n" );
-    printf( "Number of elements: %d\n", num ) ;
 
-    for( int i = 0; i < num; i++ ) {
-        printf( "%d ",data[ i ] ) ;
-
-    }//end for
-
-    
-
-
-    free( data ) ;
+    delete[] data ;
     return 0 ;
 }//end function
 
 
+int *GetSet( int *num ) {
+    printf("Enter the number of elements: ") ;
+    scanf("%d", num ) ;
+    int *data = new int[ *num ] ; 
 
-/** 
-  Function Name : GetSet pointer int
-  @Param Input *n   int  data
- 
-**/
-
-int *GetSet( int *n ) {
-    printf( "Enter the number of elements: " ) ;
-    scanf( "%d", n ) ;
-    int *arr = ( int* )malloc( *n * sizeof( int ) ) ;
-    
-    printf( "Enter the elements: ") ;
-    for(int i = 0 ; i < *n; i++ ) {
-        scanf( "%d",&arr[ i ] ) ;
+    for ( int i = 0; i < *num; i++ ) {
+        scanf("%d", &data[ i ] ) ; 
     }//end for
 
+  
+    return data ;
 
-    return arr ;
 }//end function
