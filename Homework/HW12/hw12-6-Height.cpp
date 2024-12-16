c9a77cfdd4450ebda1e87d09c14a27658b02eb65
Zag <za12ew44zz_999@hotmail.com>
2024-12-17 01:06:55 +0700

    hw12-6 not fi

--- Homework/HW12/hw12-6-Height.cpp
+++ Homework/HW12/hw12-6-Height.cpp
@@ -1,1 +1,56 @@
 #include <stdio.h>
+
+struct student{
+
+    char name[ 20 ] ;
+    int age ;
+};
+
+
+
+void GetStudent( struct student child[2][ 10 ] ) ;
+
+int main() {
+    struct student children[ 2 ][ 10 ] ;
+    
+    GetStudent( children ) ;
+
+ 
+
+  
+
+    return 0 ;
+}//end function
+
+
+void GetStudent( struct student child[][ 10 ], int *room ) {
+
+    scanf( "%d", room ) ;
+    for (int i = 0; i < *room; i++)
+    {
+        printf("Room %d:\n",*room);
+        for (int j = 0; j < 10; j++)
+        {
+            printf( "Student [%d] :",j + 1 ) ;
+            scanf("%s %d",&child[i][j].name,&child[i][j].age);
+        }//end for
+        
+        printf( "\n" ) ;
+        
+        
+        
+    }//end for
+    
+  
+   
+    
+    
+
+}//end function
+
+
+ 
+
+/////////////////////
+//a /a  / b /c  /   /   /  /
+/////////////////////
