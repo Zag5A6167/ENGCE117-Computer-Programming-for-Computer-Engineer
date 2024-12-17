7c021e87c9eab29edc0a8e25fcd3f7f00de69b97
Zag <za12ew44zz_999@hotmail.com>
2024-12-17 19:58:11 +0700

    add hw12-6

--- Homework/HW12/hw12-6-Height.cpp
+++ Homework/HW12/hw12-6-Height.cpp
@@ -2,50 +2,110 @@
 #include <string.h>
 
 struct student {
-    char name[20];
-    int age;
-};
+    char name[ 20 ] ;
+    float height ;
+} ;
 
-void GetStudent(struct student child[][10], int *numA, int *numB) {
-    int roomA = 0, roomB = 1;
-    int indexRoomA = 0, indexRoomB = 0;
-    numA = &indexRoomA;
-    numB = &indexRoomB;
-    while (1) {
-        printf("Name for Room A %d: ", indexRoomA + 1);
-        scanf("%s", child[roomA][indexRoomA].name);
+void GetStudent(struct student child[][ 10 ] ) {
+    int roomA = 0, roomB = 1 ;
+    int indexRoomA = 0, indexRoomB = 0 ;
+    printf( "Room A\n" ) ;
+    while (1) { 
 
-        if (strcmp(child[roomA][indexRoomA].name, "-1") == 0) {
-            break;
-        }
 
-        printf("Age for Room A %d: ", indexRoomA + 1);
-        scanf("%d", &child[roomA][indexRoomA].age);
+        printf( "Name: " ) ;
+        scanf( "%s", child[ roomA ][ indexRoomA ].name ) ;
 
+        if ( strcmp( child[ roomA ][ indexRoomA ].name, "-1" ) == 0 ) {
+            break ;
+        }//end if
+
+        printf( "Height (cm): ", indexRoomA + 1 ) ;
+        scanf( "%f", &child[ roomA ][ indexRoomA ].height ) ;
+
+
+        indexRoomA++ ;
+    }
+    printf( "Room B \n" ) ;
+    while (1)
+    {
+        ////  Room b
+
+        printf( "Name: " ) ;
+        scanf( "%s", child[ roomB ][ indexRoomB ].name ) ;
+
+        if ( strcmp( child[ roomB ][ indexRoomB ].name, "-1" ) == 0) {
+            break ;
+        }//end if
+
+        printf( "Height (cm): ", indexRoomB + 1 ) ;
+        scanf( "%f", &child[ roomB ][ indexRoomB ].height ) ;
        
 
-        indexRoomA++;
-    }
-
-    
-}
+        indexRoomB++ ;
+    }//end while
+    printf( "\n" ) ;
+}//end function
 
 int main() {
-    struct student children[2][10];
+    struct student children[ 2 ][ 10 ] ;
+    struct student allStudent[ 20 ] ;
+    int count = 0 ; // for all student
+    GetStudent( children ) ;
 
-    int numMemberA;
-    int numMemberB;
+    printf( "\n" ) ; 
 
-    GetStudent(children,&numMemberA,&numMemberB);
+    printf( "Room A -> " ) ;
+    for ( int i = 0; i < 10; i++ ) {
+        if ( strcmp( children[ 0 ][ i ].name, "-1" ) == 0 ) {
+            break;
+        }
+        printf( "%s ", children[ 0 ][ i ].name ) ;
+    }//end for
+    
+    printf( "\n" ) ;
+
+    //////////// Room b
+  
+    printf( "Room B -> " ) ;
+    for ( int i = 0; i < 10; i++ ) {
+        if ( strcmp( children[ 1 ][ i ].name, "-1" ) == 0 ) {
+            break ;
+        }//end if
+        printf( "%s ", children[ 1 ][ i ].name ) ;
+    }//end for
+
     
 
-    printf("\nRoom A Students:\n");
-    for (int i = 0; i < numMemberA; i++) {
-        printf("Name: %s, Age: %d\n", children[0][i].name, children[0][i].age);
-    }
+    // every student to another array 
+    for ( int i = 0; i < 2; i++ ) {
+        for ( int j = 0; j < 10; j++ ) {
+            if ( strcmp( children[ i ][ j ].name, "-1" ) == 0) {
+                break ;
+            }//end if
+            allStudent[ count++ ] = children[ i ][ j ] ;
+        }//end for
+    }//end for
 
-   
+    
+    for ( int i = 0; i < count - 1; i++ ) {
+        for ( int j = 0; j < count - i - 1; j++ ) {
+            if (allStudent[ j ].height < allStudent[ j + 1 ].height ) {
+                struct student temp = allStudent[ j ] ;
+                allStudent[ j ] = allStudent[ j + 1 ] ;
+                allStudent[ j + 1 ] = temp ;
+            }//end if
+        }//end for
+    }//end for
 
+    printf( "\n" ) ;
+    printf( "School ->" ) ;
+
+    for ( int i = 0; i < count-1; i++ ) {
+       printf( "%s ", allStudent[ i ].name ) ;
+        
+    }//end for
+    
     return 0;
 }
 
