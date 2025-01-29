7bab07dd0e9369ed52f956a3b9e3542b33375291
za12ew44zz <za12ew44zz_999@hotmail.com>
2025-01-30 00:25:56 +0700

    add lab2

--- lab-evolution-test/1-2Function passing arrays and function passing strings/lab2.cpp
+++ lab-evolution-test/1-2Function passing arrays and function passing strings/lab2.cpp
@@ -1,6 +1,40 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char* reverse( char str1[]) ;
 
 int main() {
-    //YOUR CODE HERE
-    return 0 ;
+    char text[ 50 ] ;
+    char *output ;
+    output = reverse( text ) ;
+    printf( "MAIN : %s\n", output ) ;
 }//end function
+
+
+
+
+
+
+
+char* reverse(char str1[]){
+    static char temp[50];
+    fgets(temp,sizeof(temp),stdin);
+    temp[strcspn(temp, "\n")] = 0;
+    strcpy(str1,temp);
+    int len = strlen(str1);
+
+    char *rev_str = (char*)malloc(sizeof(char) * (len + 1));
+
+    
+    for(int i = 0 ; i < len;i++){
+        rev_str[i] = str1[len - 1 - i];
+    }
+    rev_str[len] = '\0';
+
+    printf("FUNC : %s\n", rev_str);
+    return rev_str;
+}
+
+
+
+
