fb600c46696e798f4774e686f53a9e4a4f48bf6f
zag <za12ew44zz_999@hotmail.com>
2025-01-31 21:06:14 +0700

    add lab9

--- lab-evolution-test/10Recursive function/lab10.cpp
+++ lab-evolution-test/10Recursive function/lab10.cpp
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
+int SumLoop( int ) ;
+int SumRecur( int ) ;
+void TowerHanoi( int m, int i, int j ) ;
+
 int main() {
-    //YOUR CODE HERE
+    int n = 0, m = 0, i = 0, j = 0 ;
+    scanf( "%d %d %d %d", &n, &m, &i, &j ) ;
+    printf( "%d\n", SumLoop( n ) ) ;
+    printf( "%d\n", SumRecur( n ) ) ;
+    TowerHanoi( m, i, j ) ;
     return 0 ;
+}//end function
+
+int SumLoop( int n ) {
+    int sum = 0 ;
+  for(int i = 1;i <= n;i++){
+    sum += i;
+  }
+  return sum;
+}
+
+int SumRecur( int n ) {
+  if( n == 0 ) {
+    return 0 ;
+  }else {
+    return n + SumRecur( n-1 ) ;
+  }
+}//end function
+
+void TowerHanoi( int m, int i, int j ) {
+    if( m > 0 ){
+        int k = 6 - i - j ;
+        TowerHanoi(m-1,i,k ) ;
+        printf("Move disc %d from %d to %d\n",m,i,j ) ;
+        TowerHanoi( m-1,k,j ) ;
+    }
 }//end function
